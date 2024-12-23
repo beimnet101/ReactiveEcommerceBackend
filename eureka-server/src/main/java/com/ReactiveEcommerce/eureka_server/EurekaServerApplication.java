84c5b6af6e8ecdd19e4dc1877fd7428deafaf5c9
belkysupreme22 <107432127+belkysupreme22@users.noreply.github.com>
2024-12-23 14:44:35 +0300

    eureka configured

--- eureka-server/src/main/java/com/ReactiveEcommerce/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ReactiveEcommerce/eureka_server/EurekaServerApplication.java
@@ -1,9 +1,12 @@
 package com.ReactiveEcommerce.eureka_server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +14,4 @@ public class EurekaServerApplication {
 	}
 
 }
+
