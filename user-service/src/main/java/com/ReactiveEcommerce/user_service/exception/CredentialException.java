57d91b4c1c009c5be12aef6e7c3253fa74e77aee
belkysupreme22 <107432127+belkysupreme22@users.noreply.github.com>
2024-12-25 15:37:34 +0300

    Delete user-service directory

--- user-service/src/main/java/com/ReactiveEcommerce/user_service/exception/CredentialException.java
+++
@@ -1,8 +0,0 @@
-package com.ReactiveEcommerce.user_service.exception;
-
-public class CredentialException extends RuntimeException{
-
-    public CredentialException(String message) {
-        super(message);
-    }
-}
